7976915a88aca6e7c0923c24a66eef97033da7c4
lifetext <hitcopywr@gmail.com>
2023-07-23 17:19:35 +0300

    merged main & site

---
+++ js/includeFiles.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  $(function () {
+    $("#includeHead").load("head.html");
+    $("#includeHeader").load("header.html");
+    $("#includeFooter").load("footer.html");
+  });
+});
